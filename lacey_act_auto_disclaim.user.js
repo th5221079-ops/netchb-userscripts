91dc47a4b7b78f0f582e2c5e1f06b052cf8c67fb
th5221079-ops <th5221079@gmail.com>
2025-11-08 16:00:52 -0800

    Update lacey_act_auto_disclaim.user.js

--- lacey_act_auto_disclaim.user.js
+++ lacey_act_auto_disclaim.user.js
@@ -8,8 +8,8 @@
 // @run-at       document-idle
 // @grant        GM_log
 // @grant        unsafeWindow
-// @updateURL   https://raw.githubusercontent.com/th5221079-ops/netchb-userscripts/main/lacey_act_auto_disclaim.user.js
-// @downloadURL https://raw.githubusercontent.com/th5221079-ops/netchb-userscripts/main/lacey_act_auto_disclaim.user.js
+// @updateURL   https://github.com/th5221079-ops/netchb-userscripts/releases/latest/download/lacey_act_auto_disclaim.user.js
+// @downloadURL https://github.com/th5221079-ops/netchb-userscripts/releases/latest/download/lacey_act_auto_disclaim.user.js
 // ==/UserScript==
 (function () {
   'use strict';
